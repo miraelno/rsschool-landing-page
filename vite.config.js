e3560b9098c30e8e1577dacd4d9afcc1d0b38b03
Karina Bohdanova <bogdanova.karina22@gmail.com>
2026-09-20 17:07:39 +0300

    feat: init starter of the project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    root: 'src',
+    base: '/rsschool-landing-page/',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(import.meta.dirname, 'src/index.html'),
+            },
+        },
+    },
+});
